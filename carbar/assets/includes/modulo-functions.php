8e0430d762e8bb2c94719379d7263d03d1d83a70
Alringo96 <alringo96@gmail.com>
2026-05-13 15:38:41 -0400

    Delete carbar directory

--- carbar/assets/includes/modulo-functions.php
+++
@@ -1,4 +0,0 @@
-<?php
-include get_template_directory() . '/assets/modulos/modulo-taxonimia/core-modulo-taxo.php';
-
-
--- carbar/assets/librerias/js/descripcion.js
+++
@@ -1,8 +0,0 @@
-    const btn = document.querySelector('.descripcion .ver-mas');
-    const p = document.querySelector('.descripcion p');
-    if (btn && p) {
-        btn.addEventListener('click', () => {
-            p.classList.toggle('expandido');
-            btn.textContent = p.classList.contains('expandido') ? 'Ver menos' : 'Ver más';
-        });
-    }
